db63bda4bc2f89f5052a0b55a7758abff8db5b62
Nabilsping <87876387+Nabilsping@users.noreply.github.com>
2025-11-05 20:26:12 +0100

    v1

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Décrire les fonctionnalités
    |- Toutes les régle de base du uno (https://www.jeuxuno.com/regles-officielles)
 Scénario d'utilisation
    |- Chaque joueur reçoit 7 cartes choisies aléatoirement dans la pioche, toutes couleurs et valeurs confondues. Le joueur pose une carte si elle correspond en couleur ou en valeur à la carte sur le dessus du tas de défausse, sinon il pioche une carte, si la carte piochée est jouable, il peut la poser tout de suite. Si la carte du dessus de la défausse est Rouge 5, le joueur peut jouer toute carte Rouge ou toute carte 5 (ou une carte spéciale compatible).
+
+Dépendance
+pip install pygame
+
--- uno.py
+++ uno.py
@@ -3,7 +3,8 @@
 # Date : 05.11.2025
 import random
 import os 
-
+import pygame
+import sys
 os.system('cls' if os.name == 'nt' else 'clear')
 Card_List = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 Color_Cards = ["Rouge", "Bleu", "Vert", "Jaune"]
@@ -24,6 +25,32 @@ def gen_card():
             a = random.choice(Card_List)
             b = random.choice(Color_Cards)
             print(f"Couleur : {b} | Carte : {a}")
-        
+
+
+
+#========================================
+#=========PYGAME SECTION=================
+#========================================
+pygame.init()
+Largeur_fenetre = 800
+Hauteur_fenetre = 1000
+fenetre = pygame.display.set_mode((Largeur_fenetre, Hauteur_fenetre))
+
+# --- Couleurs ---
+bg_color = (50, 143, 168)
+couleur_rond = (255, 0, 0)  
+while True:
+    # Gestion des événements clavier souris fermeture
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    fenetre.fill(bg_color)  # On efface l’écran
+    pygame.draw.circle(fenetre, couleur_rond, (400, 300), 50)  
+
+    pygame.display.flip()  # Met à jour la fenêtre
+
 gen_card()
 
