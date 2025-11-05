9767010b0a44fce11d023b514920c73ce5c071aa
Nabilsping <87876387+Nabilsping@users.noreply.github.com>
2025-11-05 19:47:19 +0100

    v1

--- test.py
+++ test.py
@@ -5,25 +5,7 @@
 
 import random
 
-ValueList = ["1","2","3","4","5","6","7","8","9"]
-ColorList =["Rouge","Vert","Bleu","Jaune"]
+Card_List = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+Color_kinds = ["Rouge", "Bleu", "Vert", "Jaune"]
+Special_Cards = ["+2", "+4", "Passe ton tour"]
 
-def playerCards():
-    hand = []  # liste vide pour stocker les cartes
-    for i in range(7):
-        color = random.choice(ColorList)
-        value = random.choice(ValueList)
-        card = (color, value)
-        hand.append(card)
-    return hand
-       
-player1 = playerCards()
-player2 = playerCards()
-
-print("Cartes du Joueur 1 :")
-for color, value in player1:
-    print(f"Couleur : {color} | Valeur : {value}")
-
-print("\nCartes du Joueur 2 :")
-for color, value in player2:
-    print(f"Couleur : {color} | Valeur : {value}")
